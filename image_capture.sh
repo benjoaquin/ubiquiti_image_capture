f46744588bcc12f352baa9363aa1865074d7de61
BenJoaquin Gouverneur <benjoaquin@hey.com>
2020-10-31 22:07:27 -0700

    * adds a check to only capture between 6AM and 6PM * adds improved logging * updates the default devices

--- image_capture.sh
+++ image_capture.sh
@@ -4,19 +4,26 @@
 wd=$(pwd) # current directory where the script is 
 logs=$wd/logs # the directory where logs should go
 destination=$wd/downloads # the directory where files should go
-#devices=("back_yard" "front_house" "front_yard" "rear_house" "north_side_yard" "south_side_yard") # array of devices that must match array in installer
-devices=("garage" "living_room")
-#device_ip=("192.168.1.10" "192.168.1.23" "192.168.1.18" "192.168.1.19" "192.168.1.11" "192.168.1.21")
-device_ip=("192.168.0.120" "192.168.0.121")
+devices=("back_yard" "front_house" "front_yard" "rear_house" "north_side_yard" "south_side_yard") # array of devices that must match array in installer
+#devices=("garage" "living_room")
+device_ip=("192.168.1.10" "192.168.1.23" "192.168.1.18" "192.168.1.19" "192.168.1.11" "192.168.1.21")
+#device_ip=("192.168.0.120" "192.168.0.121")
 
 
 # This will just run until a crash or you type Ctrl + C
 while true; do
-	for index in "${!devices[@]}";
-	do 
-    	device=${devices[$index]}
-    	URL=${device_ip[$index]}
-    	wget $URL/snap.jpeg --append-output=$logs/logs.txt --output-document=$destination/$device/$device-$(date "+%Y-%m-%dT%H.%M.%S").png --timeout=10 --tries=2 --no-clobber
-	done
+	currenttime=$(date +%H:%M)
+	if [[ "$currenttime" > "06:00" ]] && [[ "$currenttime" < "18:00" ]] 
+	then
+		echo "$(date "+%Y-%m-%dT%H.%M.%S") | Attempting to capture images." >> $logs/logs.txt
+		for index in "${!devices[@]}";
+		do 
+	    	device=${devices[$index]}
+	    	URL=${device_ip[$index]}
+	    	wget $URL/snap.jpeg --append-output=$logs/logs.txt --output-document=$destination/$device/$device-$(date "+%Y-%m-%dT%H.%M.%S").png --timeout=10 --tries=2 --no-clobber
+	    done
+	else
+		echo "$(date "+%Y-%m-%dT%H.%M.%S") | Skipping capture while outside designated hours" >> $logs/logs.txt
+	fi
 	sleep 60s
-done
+done 
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#devices=("back_yard" "front_house" "front_yard" "rear_house" "north_side_yard" "south_side_yard") # array of devices that must match array in image_capture
-devices=("garage" "living_room")
+devices=("back_yard" "front_house" "front_yard" "rear_house" "north_side_yard" "south_side_yard") # array of devices that must match array in image_capture
+#devices=("garage" "living_room")
 
 mkdir ./downloads
 mkdir ./logs
