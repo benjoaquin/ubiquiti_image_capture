a1ec7e5ef09be432ddf7572ae09b1030a3a31c49
BenJoaquin Gouverneur <benjoaquin@hey.com>
2020-11-07 22:31:11 -0800

    * makes logs less verbose while retain useful details

--- image_capture.sh
+++ image_capture.sh
@@ -5,9 +5,9 @@ wd=$(pwd) # current directory where the script is
 logs=$wd/logs # the directory where logs should go
 destination=$wd/downloads # the directory where files should go
 devices=("back_yard" "front_house" "front_yard" "rear_house" "north_side_yard" "south_side_yard") # array of devices that must match array in installer
-#devices=("garage" "living_room")
 device_ip=("192.168.1.10" "192.168.1.23" "192.168.1.18" "192.168.1.19" "192.168.1.11" "192.168.1.21")
-#device_ip=("192.168.0.120" "192.168.0.121")
+# devices=("garage" "living_room")
+# device_ip=("192.168.0.120" "192.168.0.121")
 
 
 # This will just run until a crash or you type Ctrl + C
@@ -20,7 +20,7 @@ while true; do
 		do 
 	    	device=${devices[$index]}
 	    	URL=${device_ip[$index]}
-	    	wget $URL/snap.jpeg --append-output=$logs/logs.txt --output-document=$destination/$device/$device-$(date "+%Y-%m-%dT%H.%M.%S").png --timeout=10 --tries=2 --no-clobber
+	    	wget --append-output=$logs/logs.txt --output-document=$destination/$device/$device-$(date "+%Y-%m-%dT%H.%M.%S").jpeg --timeout=10 --tries=2 --no-clobber --progress=bar:force:noscroll $URL/snap.jpeg
 	    done
 	else
 		echo "$(date "+%Y-%m-%dT%H.%M.%S") | Skipping capture while outside designated hours" >> $logs/logs.txt
